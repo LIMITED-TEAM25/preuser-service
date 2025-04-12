7848669ee6e1385ae18265960d182af642825d8a
AnJungGeon <gunn0128@gmail.com>
2025-04-12 19:24:33 +0900

    🩹 fix[#1] : 체험단 컨트롤러 요청 경로 변경 - 체험단 컨트롤러 게이트웨이 설정에 맞게 변경했습니다

--- src/main/java/com/sparta/limited/preuser_service/preuser/presentation/PreuserController.java
+++ src/main/java/com/sparta/limited/preuser_service/preuser/presentation/PreuserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/preuser")
+@RequestMapping("/api/v1/preusers/")
 public class PreuserController {
 
     private final PreuserService preuserService;
