e42b5902640efc50a86dce0e10fde4e6d98684ae
AnJungGeon <gunn0128@gmail.com>
2025-04-13 01:36:49 +0900

    ✨ feat[#1] : 체험단 상태 Enum 명시 - 체험단 상태 명시적으로 추가했습니다

--- src/main/java/com/sparta/limited/preuser_service/preuser/domain/status/PreuserStatus.java
+++ src/main/java/com/sparta/limited/preuser_service/preuser/domain/status/PreuserStatus.java
@@ -1,7 +1,17 @@
 package com.sparta.limited.preuser_service.preuser.domain.status;
 
 public enum PreuserStatus {
-    RECRUITING,
-    CLOSED,
-    PLANNED,
+    RECRUITING("모집중"),
+    CLOSED("종료"),
+    PLANNED("예정"),
+    ;
+    private final String description;
+
+    PreuserStatus(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
