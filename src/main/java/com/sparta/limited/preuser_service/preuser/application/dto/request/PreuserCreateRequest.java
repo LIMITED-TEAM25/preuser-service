761b1357387ce8e3547b96bc5e0e99224b22c2bd
AnJungGeon <gunn0128@gmail.com>
2025-04-12 18:03:33 +0900

    ✨ feat[#1] : 체험단 생성 요청 dto 생성 - 체험단 생성 요청 dto 생성했습니다

---
+++ src/main/java/com/sparta/limited/preuser_service/preuser/application/dto/request/PreuserCreateRequest.java
@@ -0,0 +1,46 @@
+package com.sparta.limited.preuser_service.preuser.application.dto.request;
+
+
+import com.sparta.limited.preuser_service.preuser.domain.limit.GenderLimit;
+import com.sparta.limited.preuser_service.preuser.domain.status.PreuserStatus;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Getter;
+
+@Getter
+public class PreuserCreateRequest {
+
+    @NotNull(message = "체험단 제목은 필수 입니다")
+    private String preuserTitle;
+
+    @NotNull(message = "체험단 인원 수는 필수 입니다")
+    private int preuserCount;
+
+    private GenderLimit genderLimit;
+
+    private int ageLimit;
+
+    @NotNull(message = "체험단 상품은 필수 입니다")
+    private UUID preuserProductId;
+
+    @NotNull(message = "체험단 모집 시작일은 필수 입니다")
+    private LocalDateTime recruitStartAt;
+    
+    @NotNull(message = "체험단 모집 마감일은 필수 입니다")
+    private LocalDateTime recruitEndAt;
+
+    @NotNull(message = "체험단 시작일은 필수 입니다")
+    private LocalDateTime preuserStartAt;
+
+    @NotNull(message = "체험단 종료일은 필수 입니다")
+    private LocalDateTime preuserEndAt;
+
+    @NotNull(message = "체험단 상태는 필수입니다")
+    private PreuserStatus preuserStatus;
+
+    @NotNull(message = "체험단 발표일은 필수입니다")
+    private LocalDateTime announceDate;
+
+
+}
