6b27e80c92b12e93fb0ecb193e0d3db1dc52e28c
AnJungGeon <gunn0128@gmail.com>
2025-04-12 18:10:04 +0900

    ✨ feat[#1] : 체험자 Entity 생성했습니다 - 체험자 Entity 생성했습니다

---
+++ src/main/java/com/sparta/limited/preuser_service/preuser/domain/model/PreuserUser.java
@@ -0,0 +1,35 @@
+package com.sparta.limited.preuser_service.preuser.domain.model;
+
+import com.sparta.limited.common_module.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "p_preuser_user")
+public class PreuserUser extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @ManyToOne
+    @JoinColumn(name = "preuser_id", nullable = false)
+    private Preuser preuser;
+
+
+}
