be5436acabdf358493e50494df4f34d3843c6274
AnJungGeon <gunn0128@gmail.com>
2025-04-12 17:59:44 +0900

    ✨ feat[#1] : JpaAuditing 추가 - JpaAuditing 사용을 위한 어노테이션 추가했습니다

--- src/main/java/com/sparta/limited/preuser_service/PreuserServiceApplication.java
+++ src/main/java/com/sparta/limited/preuser_service/PreuserServiceApplication.java
@@ -2,12 +2,14 @@ package com.sparta.limited.preuser_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PreuserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PreuserServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PreuserServiceApplication.class, args);
+    }
 
 }
