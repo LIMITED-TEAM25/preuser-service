c841f697238e993a9dd0a3d1f7ed4da4e338f357
AnJungGeon <gunn0128@gmail.com>
2025-04-13 23:01:55 +0900

    ✨ feat[#4] : 체험단 단건 조회 비즈니스 로직 추가 - 체험단 서비스에 단건 조회 메서드 추가했습니다

--- src/main/java/com/sparta/limited/preuser_service/preuser/application/service/PreuserService.java
+++ src/main/java/com/sparta/limited/preuser_service/preuser/application/service/PreuserService.java
@@ -2,8 +2,12 @@ package com.sparta.limited.preuser_service.preuser.application.service;
 
 import com.sparta.limited.preuser_service.preuser.application.dto.request.PreuserCreateRequest;
 import com.sparta.limited.preuser_service.preuser.application.dto.response.PreuserCreateResponse;
+import com.sparta.limited.preuser_service.preuser.application.dto.response.PreuserGetResponse;
+import java.util.UUID;
 
 public interface PreuserService {
 
     PreuserCreateResponse createPreuser(PreuserCreateRequest request);
+
+    PreuserGetResponse getPreuser(UUID preuserId);
 }
