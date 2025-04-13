d2c5e1fa946bf106a1a81aa558715c73c834b479
AnJungGeon <gunn0128@gmail.com>
2025-04-13 21:26:23 +0900

    ✨ feat[#11] : 체험단 상품 서비스 생성 - 체험단 상품 서비스 등록 비즈니스 로직 작성했습니다

---
+++ src/main/java/com/sparta/limited/preuser_service/preuser_product/application/servcie/PreuserProductServiceImpl.java
@@ -0,0 +1,44 @@
+package com.sparta.limited.preuser_service.preuser_product.application.servcie;
+
+import com.sparta.limited.preuser_service.preuser_product.application.dto.request.PreuserProductCreateRequest;
+import com.sparta.limited.preuser_service.preuser_product.application.dto.response.PreuserProductCreateResponse;
+import com.sparta.limited.preuser_service.preuser_product.application.mapper.PreuserProductMapper;
+import com.sparta.limited.preuser_service.preuser_product.domain.model.PreuserProduct;
+import com.sparta.limited.preuser_service.preuser_product.domain.repository.PreuserProductRepository;
+import com.sparta.limited.preuser_service.preuser_product.infrastructure.client.ProductClient;
+import com.sparta.limited.preuser_service.preuser_product.infrastructure.dto.response.ProductReadResponse;
+import jakarta.transaction.Transactional;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PreuserProductServiceImpl implements PreuserProductService {
+
+    private final PreuserProductRepository preuserProductRepository;
+    private final ProductClient productClient;
+
+    @Override
+    @Transactional
+    public PreuserProductCreateResponse createPreuserProduct(PreuserProductCreateRequest request) {
+
+        UUID productId = request.getProductId();
+
+        preuserProductRepository.findByProductId(productId);
+
+        ProductReadResponse product = productClient.getProduct(productId);
+
+        PreuserProduct preuserProduct = PreuserProduct.ofFegin(
+            product.getTitle(),
+            product.getDescription(),
+            product.getPrice(),
+            product.getId(),
+            request.getQuantity()
+        );
+
+        preuserProductRepository.save(preuserProduct);
+
+        return PreuserProductMapper.toPreuserProductCreateResponse(preuserProduct);
+    }
+}
