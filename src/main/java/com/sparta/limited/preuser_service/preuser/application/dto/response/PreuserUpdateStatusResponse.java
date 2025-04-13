1f17b2b274c9ce63fa7be6b7df75b9d4ab06cb04
AnJungGeon <gunn0128@gmail.com>
2025-04-14 02:46:43 +0900

    ✨feat[#14] : 체험단 상태 수정 응답 dto 생성 - 체험단 상태 수정 응답 dto 작성했습니다

---
+++ src/main/java/com/sparta/limited/preuser_service/preuser/application/dto/response/PreuserUpdateStatusResponse.java
@@ -0,0 +1,32 @@
+package com.sparta.limited.preuser_service.preuser.application.dto.response;
+
+import com.sparta.limited.preuser_service.preuser.domain.status.PreuserStatus;
+import java.util.UUID;
+import lombok.Getter;
+
+@Getter
+public class PreuserUpdateStatusResponse {
+
+    private UUID preuserId;
+    private String preuserTitle;
+    private PreuserStatus preuserStatus;
+
+    public PreuserUpdateStatusResponse(
+        UUID preuserId,
+        String preuserTitle,
+        PreuserStatus preuserStatus) {
+        this.preuserId = preuserId;
+        this.preuserStatus = preuserStatus;
+        this.preuserTitle = preuserTitle;
+    }
+
+    public static PreuserUpdateStatusResponse of(
+        UUID preuserId,
+        String preuserTitle,
+        PreuserStatus preuserStatus) {
+        return new PreuserUpdateStatusResponse(
+            preuserId,
+            preuserTitle,
+            preuserStatus);
+    }
+}
