9629835f1332aac7e1857da99852359fb3d2879c
AnJungGeon <gunn0128@gmail.com>
2025-04-14 19:50:12 +0900

    ✨feat[#9] : 체험단 전체 조회 응답 Mapper 작성 - 체험단 전체 조회 응답 변환 Mapper 작성했습니다

--- src/main/java/com/sparta/limited/preuser_service/preuser/application/mapper/PreuserMapper.java
+++ src/main/java/com/sparta/limited/preuser_service/preuser/application/mapper/PreuserMapper.java
@@ -2,6 +2,7 @@ package com.sparta.limited.preuser_service.preuser.application.mapper;
 
 import com.sparta.limited.preuser_service.preuser.application.dto.request.PreuserCreateRequest;
 import com.sparta.limited.preuser_service.preuser.application.dto.response.PreuserCreateResponse;
+import com.sparta.limited.preuser_service.preuser.application.dto.response.PreuserGetForPageResponse;
 import com.sparta.limited.preuser_service.preuser.application.dto.response.PreuserGetResponse;
 import com.sparta.limited.preuser_service.preuser.application.dto.response.PreuserUpdateStatusResponse;
 import com.sparta.limited.preuser_service.preuser.domain.model.Preuser;
@@ -14,42 +15,49 @@ public class PreuserMapper {
 
     public static PreuserUpdateStatusResponse toPreuserUpdateStatusResponse(Preuser preuser) {
         return PreuserUpdateStatusResponse.of(
-            preuser.getId(),
-            preuser.getPreuserTitle(),
-            preuser.getPreuserStatus()
+                preuser.getId(),
+                preuser.getPreuserTitle(),
+                preuser.getPreuserStatus()
+        );
+    }
+
+    public static PreuserGetForPageResponse toPreuserGetForPageResponse(Preuser preuser) {
+        return PreuserGetForPageResponse.of(
+                preuser.getId(),
+                preuser.getPreuserTitle()
         );
     }
 
     public static PreuserGetResponse toPreuserGetResponse(Preuser preuser) {
         return PreuserGetResponse.of(
-            preuser.getId(),
-            preuser.getPreuserTitle(),
-            preuser.getPreuserCount(),
-            preuser.getGenderLimit(),
-            preuser.getAgeLimit(),
-            preuser.getPreuserProductId(),
-            preuser.getRecruitStartAt(),
-            preuser.getRecruitEndAt(),
-            preuser.getPreuserStartAt(),
-            preuser.getPreuserEndAt(),
-            preuser.getPreuserStatus(),
-            preuser.getAnnounceDate()
+                preuser.getId(),
+                preuser.getPreuserTitle(),
+                preuser.getPreuserCount(),
+                preuser.getGenderLimit(),
+                preuser.getAgeLimit(),
+                preuser.getPreuserProductId(),
+                preuser.getRecruitStartAt(),
+                preuser.getRecruitEndAt(),
+                preuser.getPreuserStartAt(),
+                preuser.getPreuserEndAt(),
+                preuser.getPreuserStatus(),
+                preuser.getAnnounceDate()
         );
     }
 
     public static Preuser toEntity(PreuserCreateRequest request) {
         return Preuser.of(
-            request.getPreuserTitle(),
-            request.getPreuserCount(),
-            request.getGenderLimit(),
-            request.getAgeLimit(),
-            request.getPreuserProductId(),
-            request.getRecruitStartAt(),
-            request.getRecruitEndAt(),
-            request.getPreuserStartAt(),
-            request.getPreuserEndAt(),
-            request.getPreuserStatus(),
-            request.getAnnounceDate()
+                request.getPreuserTitle(),
+                request.getPreuserCount(),
+                request.getGenderLimit(),
+                request.getAgeLimit(),
+                request.getPreuserProductId(),
+                request.getRecruitStartAt(),
+                request.getRecruitEndAt(),
+                request.getPreuserStartAt(),
+                request.getPreuserEndAt(),
+                request.getPreuserStatus(),
+                request.getAnnounceDate()
         );
     }
 }
