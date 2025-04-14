2b506c2dd14480eb5ea3f116096a054d4fef3148
AnJungGeon <gunn0128@gmail.com>
2025-04-14 19:49:07 +0900

    ✨feat[#9] : 체험단 전체 조회 컨트롤러 작성 - 체험단 전체 조회 요청 컨트롤러 작성했습니다

--- src/main/java/com/sparta/limited/preuser_service/preuser/presentation/PreuserController.java
+++ src/main/java/com/sparta/limited/preuser_service/preuser/presentation/PreuserController.java
@@ -3,24 +3,22 @@ package com.sparta.limited.preuser_service.preuser.presentation;
 import com.sparta.limited.preuser_service.preuser.application.dto.request.PreuserCreateRequest;
 import com.sparta.limited.preuser_service.preuser.application.dto.request.PreuserUpdateStatusRequest;
 import com.sparta.limited.preuser_service.preuser.application.dto.response.PreuserCreateResponse;
+import com.sparta.limited.preuser_service.preuser.application.dto.response.PreuserGetForPageResponse;
 import com.sparta.limited.preuser_service.preuser.application.dto.response.PreuserGetResponse;
 import com.sparta.limited.preuser_service.preuser.application.dto.response.PreuserUpdateStatusResponse;
 import com.sparta.limited.preuser_service.preuser.application.service.PreuserService;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/preusers/")
+@RequestMapping("/api/v1/preusers")
 public class PreuserController {
 
     private final PreuserService preuserService;
@@ -28,30 +26,39 @@ public class PreuserController {
 
     @PostMapping
     public ResponseEntity<PreuserCreateResponse> createPreuser(
-        @RequestBody PreuserCreateRequest request) {
+            @RequestBody PreuserCreateRequest request) {
         PreuserCreateResponse response = preuserService.createPreuser(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @GetMapping("{preuserId}")
+    @GetMapping("/{preuserId}")
     public ResponseEntity<PreuserGetResponse> getPreuser(
-        @PathVariable UUID preuserId
+            @PathVariable UUID preuserId
     ) {
         PreuserGetResponse response = preuserService.getPreuser(preuserId);
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    @PatchMapping("{preuserId}/status")
+    @PatchMapping("/{preuserId}/status")
     public ResponseEntity<PreuserUpdateStatusResponse> updatePreuserStatus(
-        @PathVariable UUID preuserId,
-        @RequestBody PreuserUpdateStatusRequest preuserStatus
+            @PathVariable UUID preuserId,
+            @RequestBody PreuserUpdateStatusRequest preuserStatus
     ) {
         PreuserUpdateStatusResponse response = preuserService.updatePreuserStatus(preuserId,
-            preuserStatus.getPreuserStatus());
+                preuserStatus.getPreuserStatus());
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<PreuserGetForPageResponse>> getAllPreuser(
+            Pageable pageable
+    ) {
+        Page<PreuserGetForPageResponse> responses = preuserService.getAllPreuesr(pageable);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responses);
     }
 
 }
