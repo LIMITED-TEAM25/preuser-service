895d2b005de250010be6acc128708674fa9f72da
AnJungGeon <gunn0128@gmail.com>
2025-04-14 22:38:46 +0900

    ✨ feat[#12] : 체험 상품 단건 조회 컨트롤러 작성 - 체험 상품 단건 조회 요청 메소드 작성했습니다

--- src/main/java/com/sparta/limited/preuser_service/preuser_product/presentation/PreuserProductController.java
+++ src/main/java/com/sparta/limited/preuser_service/preuser_product/presentation/PreuserProductController.java
@@ -2,17 +2,19 @@ package com.sparta.limited.preuser_service.preuser_product.presentation;
 
 import com.sparta.limited.preuser_service.preuser_product.application.dto.request.PreuserProductCreateRequest;
 import com.sparta.limited.preuser_service.preuser_product.application.dto.response.PreuserProductCreateResponse;
+import com.sparta.limited.preuser_service.preuser_product.application.dto.response.PreuserProductGetResponse;
 import com.sparta.limited.preuser_service.preuser_product.application.servcie.PreuserProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/api/v1/preuser-products/")
 public class PreuserProductController {
 
@@ -20,9 +22,19 @@ public class PreuserProductController {
 
     @PostMapping
     public ResponseEntity<PreuserProductCreateResponse> createPreuserProduct(
-        @RequestBody PreuserProductCreateRequest request) {
+            @RequestBody PreuserProductCreateRequest request) {
         PreuserProductCreateResponse response = preuserProductService.createPreuserProduct(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+    @GetMapping("{preuserProductId}")
+    public ResponseEntity<PreuserProductGetResponse> getPreuserProduct(
+            @PathVariable UUID preuserProductId
+    ) {
+        PreuserProductGetResponse response = preuserProductService.getPreuserProduct(preuserProductId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
 
 }
