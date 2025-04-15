a8c8841355823f6e2e45e55ae0034aa5cb165d65
AnJungGeon <gunn0128@gmail.com>
2025-04-16 01:44:14 +0900

    ✨ feat[#10] : 체험단 선정자 필드 추가 - 체험단 선정자 구분과 이미 선정한 체험단인지 구분하기 위한 필드 추가했습니다

--- src/main/java/com/sparta/limited/preuser_service/preuser/domain/model/PreuserUser.java
+++ src/main/java/com/sparta/limited/preuser_service/preuser/domain/model/PreuserUser.java
@@ -21,26 +21,36 @@ public class PreuserUser extends BaseEntity {
     @Column(name = "user_id", nullable = false)
     private Long userId;
 
+    @Column(name = "is_selected", nullable = false)
+    private boolean isSelected = false;
+
     @ManyToOne
     @JoinColumn(name = "preuser_id", nullable = false)
     private Preuser preuser;
 
     private PreuserUser(
             Long userId,
-            Preuser preuser
+            Preuser preuser,
+            boolean isSelected
     ) {
         this.userId = userId;
         this.preuser = preuser;
+        this.isSelected = isSelected;
     }
 
     public static PreuserUser of(
             Long userId,
-            Preuser preuser
+            Preuser preuser,
+            boolean isSelected
     ) {
         return new PreuserUser(
                 userId,
-                preuser
+                preuser,
+                isSelected
         );
     }
-    
+
+    public void select() {
+        this.isSelected = true;
+    }
 }
