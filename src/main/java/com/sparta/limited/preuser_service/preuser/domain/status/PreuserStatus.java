0498b543cddd69748051d186f8c40bb42f2602c1
AnJungGeon <gunn0128@gmail.com>
2025-04-16 21:25:38 +0900

    ✨ feat : 체험단 상태 정적 팩토리 메서드 생성 - 체험단 등록 시 체험단 상태를 판단하는 정적 팩토리 메서드를 생성했습니다

--- src/main/java/com/sparta/limited/preuser_service/preuser/domain/status/PreuserStatus.java
+++ src/main/java/com/sparta/limited/preuser_service/preuser/domain/status/PreuserStatus.java
@@ -1,5 +1,7 @@
 package com.sparta.limited.preuser_service.preuser.domain.status;
 
+import java.time.LocalDateTime;
+
 public enum PreuserStatus {
     RECRUITING("모집중"),
     CLOSED("종료"),
@@ -9,6 +11,10 @@ public enum PreuserStatus {
 
     PreuserStatus(String description) {
         this.description = description;
+    }
+
+    public static PreuserStatus fromRecruitStartAt(LocalDateTime recruitStartAt) {
+        return recruitStartAt.isAfter(LocalDateTime.now()) ? PLANNED : RECRUITING;
     }
 
     public String getDescription() {
