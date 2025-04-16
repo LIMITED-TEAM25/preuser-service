5622f274fda251e3e83aedbaba6bbad94c13b6f3
AnJungGeon <gunn0128@gmail.com>
2025-04-16 21:45:45 +0900

    ✨ feat : 체험단 상태 조회 Repository 작성 - 체험단 상태를 모집 마감 시간과 비교하는 메서드 작성했습니다 - 체험단 상태를 모집 시작 시간과 비교한는 메서드 작성했습니다

--- src/main/java/com/sparta/limited/preuser_service/preuser/domain/repository/PreuserRepository.java
+++ src/main/java/com/sparta/limited/preuser_service/preuser/domain/repository/PreuserRepository.java
@@ -1,9 +1,12 @@
 package com.sparta.limited.preuser_service.preuser.domain.repository;
 
 import com.sparta.limited.preuser_service.preuser.domain.model.Preuser;
+import com.sparta.limited.preuser_service.preuser.domain.status.PreuserStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface PreuserRepository {
@@ -15,4 +18,8 @@ public interface PreuserRepository {
     Page<Preuser> findAll(Pageable pageable);
 
     Preuser findWithPessimisticLockById(UUID preuserId);
+
+    List<Preuser> findByRecruitEndAtBeforeAndPreuserStatus(LocalDateTime now, PreuserStatus preuserStatus);
+
+    List<Preuser> findByRecruitStartAtBeforeAndPreuserStatus(LocalDateTime now, PreuserStatus preuserStatus);
 }
--- src/main/java/com/sparta/limited/preuser_service/preuser/infrastructure/repository/PreuserRepositoryImpl.java
+++ src/main/java/com/sparta/limited/preuser_service/preuser/infrastructure/repository/PreuserRepositoryImpl.java
@@ -4,12 +4,15 @@ import com.sparta.limited.common_module.exception.BusinessException;
 import com.sparta.limited.common_module.exception.ErrorCode;
 import com.sparta.limited.preuser_service.preuser.domain.model.Preuser;
 import com.sparta.limited.preuser_service.preuser.domain.repository.PreuserRepository;
+import com.sparta.limited.preuser_service.preuser.domain.status.PreuserStatus;
 import com.sparta.limited.preuser_service.preuser.infrastructure.persistence.JpaPreuserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -38,5 +41,15 @@ public class PreuserRepositoryImpl implements PreuserRepository {
     public Preuser findWithPessimisticLockById(UUID preuserId) {
         return jpaPreuserRepository.findWithPessimisticLockById(preuserId)
                 .orElseThrow(() -> new BusinessException(ErrorCode.RESOURCES_NOT_FOUND));
+    }
+
+    @Override
+    public List<Preuser> findByRecruitEndAtBeforeAndPreuserStatus(LocalDateTime now, PreuserStatus preuserStatus) {
+        return jpaPreuserRepository.findByRecruitEndAtBeforeAndPreuserStatus(now, preuserStatus);
+    }
+
+    @Override
+    public List<Preuser> findByRecruitStartAtBeforeAndPreuserStatus(LocalDateTime now, PreuserStatus preuserStatus) {
+        return jpaPreuserRepository.findByRecruitStartAtBeforeAndPreuserStatus(now, preuserStatus);
     }
 }
