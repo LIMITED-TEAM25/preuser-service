c23e262d4416884b17a2546fb53cef0d19ee03e4
AnJungGeon <gunn0128@gmail.com>
2025-05-01 12:54:19 +0900

    ✨ feat [#40] : 레디스 키 생성

---
+++ src/main/java/com/sparta/limited/preuser_service/preuser/infrastructure/constants/RedisKey.java
@@ -0,0 +1,9 @@
+package com.sparta.limited.preuser_service.preuser.infrastructure.constants;
+
+public final class RedisKey {
+    public static final String PREUSER = "preuser:";
+    public static final String USER = "user:";
+    private RedisKey() {
+    }
+
+}
